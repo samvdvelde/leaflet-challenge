521f293ac5645d2d8d7c0803d7098fb31ba2c657
svandevelde <78446105+samvdvelde@users.noreply.github.com>
2021-06-08 15:04:29 -0700

    changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -27,6 +27,93 @@ function createMap(quakeMap) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(map);
+
+
+    
+    // function  getColor(s) {
+    //     if ( s === '0 - 10') 
+    //         return 'blue';
+    //     else if ( s === '10 - 30') 
+    //         return 'green';
+    //     else if ( s === '30 - 50') 
+    //         return 'yellow';
+    //     else if ( s === '50 - 70') 
+    //         return 'orange';
+    //     else if ( s === '70 - 90') 
+    //         return 'brown';
+    //     else
+    //         return 'red';
+    //     }
+    // var legend = L.control({position: 'bottomright'});
+    
+    
+    // legend.onAdd = function (map) {
+    //     var legendDiv =  L.DomUtil.create('div', 'info legend'),
+    //         levels = ['0 - 10', '10 - 30', '30 - 50', '50 - 70', '70 - 90', '90+'],
+    //         title= ['<strong>Depth</strong>'],
+    //         labels = [];
+    //     for ( var i=0; i < levels.length; i++) {
+    //         labels.push( 
+    //             '<i class="square" style="background:' + getColor(levels[i]) + '"></i>'+ levels[i] + '')
+    //         }
+    //         legendDiv.innerHTML = labels.join('<br>');
+    
+    
+    //         return legendDiv;
+    //     }
+    
+    //     legend.addTo(map);
+
+      function getColor(d) {
+        return d === '0 - 10'  ? "#de2d26" :
+             d === '10 - 30'  ? "#377eb8" :
+             d === '30 - 50' ? "#4daf4a" :
+             d === '50 - 70' ? "#984ea3" :
+             d === '70 - 90' ? "#984ea3" :
+                "#984ea3" ;
+      };
+
+
+        // var legend = L.control({position: 'bottomright'});
+        // legend.onAdd = function (map) {
+    
+        // var div = L.DomUtil.create('div', 'info legend');
+        // labels = ['<strong>Depth</strong>'],
+        // categories = ['0 - 10', '10 - 30', '30 - 50', '50 - 70', '70 - 90', '90+'];
+    
+        // for (var i = 0; i < categories.length; i++) {
+    
+        //         div.innerHTML += 
+        //         labels.push(
+        //             '<i class="square" style="background:' + getColor(categories[i]) + '"></i> ' +
+        //         (categories[i] ? categories[i] : '+'));
+    
+        //     }
+        //     div.innerHTML = labels.join('<br>');
+        // return div;
+        // };
+        // legend.addTo(map);
+
+        var legend = L.control({ position: "bottomright" });
+
+        legend.onAdd = function(map) {
+          var div = L.DomUtil.create("div", "legend");
+          div.innerHTML += "<h4>Depth</h4>";
+          div.innerHTML += '<i style="background: blue"></i><span>0 - 10</span><br>';
+          div.innerHTML += '<i style="background: green"></i><span>10 - 30</span><br>';
+          div.innerHTML += '<i style="background: yellow"></i><span>30 - 50</span><br>';
+          div.innerHTML += '<i style="background: orange"></i><span>50 - 70</span><br>';
+          div.innerHTML += '<i style="background: brown"></i><span>70 - 90</span><br>';
+          div.innerHTML += '<i style="background: red"></i><span>90+</span><br>';
+
+        
+          return div;
+        };
+        
+        legend.addTo(map);
+
+
+
 };
 
 console.log(API_KEY)
@@ -82,7 +169,7 @@ function createCircles(response) {
 
         
         var quakeCircles = L.circleMarker([lat, lon], {
-            fillOpacity: 0.75,
+            fillOpacity: 0.3,
             color: "gray",
             weight: 0.5,
             fillColor: fillcolor,
@@ -90,11 +177,14 @@ function createCircles(response) {
         }).bindPopup("<h3>" + place + "<h3><h3>Magnitude: " + mag + "</h3>");
 
         circles.push(quakeCircles);
+
+
         
         }
     console.log(circles);
     createMap(L.layerGroup(circles));
 };       
+
 
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson").then(createCircles);
